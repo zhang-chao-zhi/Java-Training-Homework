5fe6ec5ce02d0db6863e341804d577815d31faef
zhang-chao-zhi <66006636+zhang-chao-zhi@users.noreply.github.com>
2022-06-22 00:26:59 +0800

    Update Input.java

--- The second week/GuessNumber/src/com/Input.java
+++ The second week/GuessNumber/src/com/Input.java
@@ -1,50 +1,36 @@
 package com;
 
 public class Input {
-    public String answer(int number){
+    public String result(int answer,int guessTotal) {
+
         // 游戏设置
         int rangeStart = 1;
         int rangeEnd = 8;
-        int guessTotal = 5;
 
-        // 游戏统计
-        int totalGameCount = 0;
-        int totalCorrectCount = 0;
+        // 初始化本次游戏的变量
+        int mod = rangeEnd - rangeStart;
+        double randNum = Math.random();
+        int num = ((int) (randNum * rangeEnd * 100)) % mod;
+        num += rangeStart;
+        if (num <= rangeStart) {
+            num = rangeStart + 1;
+        } else {
+            num = rangeEnd - 1;
+        }
 
-        // 是否结束游戏
-        boolean stopGame = false;
-
-        while (!stopGame) {
-            // 初始化本次游戏的变量
-            int guessLeft = guessTotal;
-            int mod = rangeEnd - rangeStart;
-            double randNum = Math.random();
-            int num = ((int) (randNum * rangeEnd * 100)) % mod;
-            num += rangeStart;
-            if (num <= rangeStart) {
-                num = rangeStart + 1;
-            }
-            if (num >= rangeEnd) {
-                num = rangeEnd - 1;
-            }
-
-            // 本次是否猜中数字
-            boolean guessCorrect = false;
-            while (guessLeft > 0) {
-                if (number == num) {
-                    return "恭喜你猜对了！这次的数字就是" + num;
-                } else if (number > num) {
-                    return "猜测的数字比目标数字大,继续猜";
-                } else {
-                    return "猜测的数字比目标数字小,继续猜";
-                }
-            }
-            totalCorrectCount++;
-            totalGameCount++;
-            if (!guessCorrect) {
-                return "本次的目标数字是" + num + "。这次没有猜中。" ;
+        // 本次是否猜中数字
+        while (guessTotal > 0) {
+            if (answer == num) {
+                System.out.print("恭喜你猜对了！这次的数字就是" + num );break;
+            } else if (answer > num) {
+                return "猜测的数字比目标数字大,继续猜,剩余次数为" + guessTotal + "次";
+            } else {
+                return "猜测的数字比目标数字小,继续猜,剩余次数为" + guessTotal + "次";
             }
         }
-        return "共进行了" + totalGameCount + "次游戏，其中正确的为" + totalCorrectCount + "次" ;
+        if (guessTotal == 0) {
+            return "本次的目标数字是" + num + ",这次没有猜中。";
+        }
+        return ",游戏结束！";
     }
 }
